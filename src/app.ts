9d588195a897a7df0492c1319ba0fd5ade82a49c
chad hinds <chadrickj8@gmail.com>
2023-07-09 12:07:30 -0400

    set the entry at this specific index to be newPerson

---
+++ src/app.ts
@@ -0,0 +1,62 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const PORT = 4000;
+
+// Dummy data
+const data = [
+    {
+        name: "Marc",
+        age: 45,
+        id: 1
+    },
+    {
+        name: "Matthew",
+        age: 23,
+        id: 2
+    },
+    {
+        name: "Cole",
+        age: 32,
+        id: 3
+    },
+    {
+        name: "Melony",
+        age: 39,
+        id: 4
+    }
+];
+
+// Specify the type of express thing being used
+const app: Application = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send(data);
+})
+
+app.get("/:id", (req, res) => {
+    //ID we're looking for parsed from our URL parameter, with base 10
+    const personID = parseInt(req.params.id, 10);
+    // For every 'person' in 'data', only return the person whose ID matches parameter
+    const specificPerson = data.find(person => person.id === personID);
+    // Respond with our specific person with the correct ID
+    res.send(specificPerson);
+})
+
+// "put" handler, to replace entire section of data without updating
+app.put("/:id", (req, res) => {
+    const personID = parseInt(req.params.id, 10);
+
+    console.log(`Current ID is ${personID}`);
+    const personIndex = data.findIndex(person => person.id === personID);
+    const newPerson = {
+        ...req.body,
+        id: personID
+    }
+    // set the entry at this specific index to be newPerson
+    data[personIndex] = newPerson;
+    res.send(newPerson);
+})
+
+app.listen(PORT, () => console.log(`Server bussin on port ${PORT}`));
